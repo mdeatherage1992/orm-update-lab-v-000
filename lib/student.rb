a7b014ed615bcbea788eeedad6049a989944a5ae
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 19:03:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,7 +45,7 @@ def save
 end
 
 def self.find_by_name(name)
-    sql = "SELECT * FROM songs WHERE name = ?"
+    sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
 end
