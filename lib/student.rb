943751a0424685fa80eac9e4252915db8d573f60
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-07-05 19:02:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,13 @@ def save
   student
 end
 
+def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+end
+
 
 
 
